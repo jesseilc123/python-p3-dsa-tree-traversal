8e2eb04bf976ce66a9b30b5e2832a9e679b70e22
Jesse Ilc <jesseilc123@gmail.com>
2023-06-06 15:53:26 -0500

    Lab complete

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,20 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = None
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      if node["id"] != id:
+        # depth-first traversal
+        # nodes_to_visit = node["children"] + nodes_to_visit
+        # breadth-first traversal
+        nodes_to_visit = nodes_to_visit + node["children"]
+      else:
+        result = node 
+        break
+    
+    return result
+  
+
